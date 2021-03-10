638750ba7d862c3922257e093c3bf2e4a726a427
syl20ko <sylvain.caron@baliztic.com>
2021-03-10 11:21:28 +0100

    sky drei config

--- src/App.js
+++ src/App.js
@@ -2,7 +2,7 @@ import React from "react";
 import { Canvas } from "react-three-fiber";
 import Box from "./components/box";
 import Plane from "./components/plane";
-import { OrbitControls, Stars } from "@react-three/drei";
+import { OrbitControls, Sky } from "@react-three/drei";
 import { Physics } from "@react-three/cannon";
 import "./index.css";
 
@@ -10,12 +10,13 @@ function App() {
   return (
     <Canvas camera={{ position: [-5, 2, 10], fov: 60 }}>
       <OrbitControls />
-      <Stars />
+      <Sky sunPosition={[0, 10, 0]} inclination={0.7} azimuth={0.25} />
       <ambientLight intensity={0.5} />
-      <spotLight position={[10, 15, 10]} angle={0.3} />
+      <pointLight position={[-10, 5, -20]} intensity={0.5} />
+      <pointLight position={[0, 5, 0]} intensity={1.5} />
       <Physics>
-        <Box/>
-        <Plane/>
+        <Box />
+        <Plane />
       </Physics>
     </Canvas>
   );
